f619fb21728b2f6e7711fdcaaf7a8e89ecc4e3eb
binl233 <835741923@qq.com>
2023-12-05 17:27:37 -0500

    Add Contact us Feature

--- Revive/Revive/View/Setting/ContactUs.swift
+++ Revive/Revive/View/Setting/ContactUs.swift
@@ -8,13 +8,37 @@
 import SwiftUI
 
 struct ContactUs: View {
+    @State private var isEmailCopied : Bool = false
+    
     var body: some View {
         Section {
-            
+            List {
+                HStack {
+                    Text("Email Address")
+                    Spacer()
+                    Button(action: {
+                        copyText()
+                        isEmailCopied = true
+                    }) {
+                        Text(isEmailCopied ? "Copied!" : "Copy")
+                            .font(.subheadline)
+                            .foregroundStyle(.white)
+                            .padding(8)
+                    }
+                    .background(Color.cDarkBrown)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                }
+            }
+        }
+        header: {
+           Text("Contact Us")
+        } footer: {
+           Text("Contact us if you have any problem.")
         }
     }
-}
-
-#Preview {
-    ContactUs()
+    
+    func copyText() {
+        let pasteboard = UIPasteboard.general
+        pasteboard.string = "ljreviveapp@gmail.com"
+    }
 }
