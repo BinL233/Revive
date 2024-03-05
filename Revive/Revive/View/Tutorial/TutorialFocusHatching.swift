2b5f85848c9aede9efb2cf279603720b1755c5d8
binl233 <835741923@qq.com>
2024-03-04 23:13:58 -0500

    Update new items, Create animation for store, Add functions for storeitems

---
+++ Revive/Revive/View/Tutorial/TutorialFocusHatching.swift
@@ -0,0 +1,18 @@
+//
+//  TutorialFocusHatching.swift
+//  Revive
+//
+//  Created by Kaile Ying on 3/4/24.
+//
+
+import SwiftUI
+
+struct TutorialFocusHatching: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TutorialFocusHatching()
+}
