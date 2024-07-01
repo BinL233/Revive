a66db2849be2ddb329038e588193668218f3556f
binl233 <835741923@qq.com>
2024-07-01 11:57:49 +0800

    Fixed Dialog layout Phase2

--- Revive/Revive/View/Tutorial/GuideHatching.swift
+++ Revive/Revive/View/Tutorial/GuideHatching.swift
@@ -12,35 +12,43 @@ struct GuideHatching: View {
     @Binding var currentIndex: Int
     
     var body: some View {
-        ZStack {
-            HStack {
+        GeometryReader { geometry in
+            ZStack(alignment: .topLeading) {
+                HStack {
                     Image("XX")
+                        .resizable()
+                        .scaledToFit()
+                        .shadow(radius: 6, x: 0, y: 4)
+                        .scaleEffect(x: -1, y: 1)
+                        .frame(width: 300, height: 300)
+                        .offset(x: -60, y: geometry.size.height/2 - 300)
+                    
+                    Spacer()
+                }
+                
+                Image("DialogBox")
                     .resizable()
                     .scaledToFit()
                     .shadow(radius: 6, x: 0, y: 4)
-                    .scaleEffect(x: -1, y: 1)
-                    .frame(width: 300, height: 300)
-                    .offset(x: -60, y: -60)
+                    .frame(width: geometry.size.width, height: geometry.size.height)
                 
-                Spacer()
+                if !manager.tutorialDialog2.isEmpty {
+                    Text("Xixi")
+                        .font(.custom("Georgia-Italic", size: 18))
+                        .foregroundStyle(Color.cBlack)
+                        .multilineTextAlignment(.leading)
+                        .position(x: geometry.size.width * 0.21, y: geometry.size.height * 0.423)
+                    
+                    Text(manager.tutorialDialog2[currentIndex])
+                        .font(.custom("Georgia-Italic", size: 15))
+                        .padding(30)
+                        .foregroundStyle(Color.cBlack)
+                        .multilineTextAlignment(.leading)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .offset(y: geometry.size.height * 0.44)
+                }
             }
-            
-            Image("DialogBox")
-                .resizable()
-                .scaledToFit()
-                .shadow(radius: 6, x: 0, y: 4)
-                .offset(y: 80)
-            
-            if !manager.tutorialDialog2.isEmpty {
-                Text(manager.tutorialDialog2[currentIndex])
-                    .font(.custom("Georgia-Italic", size: 15))
-                    .padding(30)
-                    .foregroundStyle(Color.cBlack)
-                    .multilineTextAlignment(.leading)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .offset(y: 80)
-            }
+            .padding(.bottom, 100)
         }
-        .padding(.bottom, 100)
     }
 }
