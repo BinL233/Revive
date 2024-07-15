56fa2c3ce1a80331acadd157c4902f294bc1fcc8
binl233 <835741923@qq.com>
2024-07-15 09:28:41 +0800

    Remove sensitive folders and update

---
+++ Revive/Revive/View/Focus/StartButtons/StrictStartButton.swift
@@ -0,0 +1,18 @@
+//
+//  StrictStartButton.swift
+//  Revive
+//
+//  Created by Kaile Ying on 7/15/24.
+//
+
+import SwiftUI
+
+struct StrictStartButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StrictStartButton()
+}
